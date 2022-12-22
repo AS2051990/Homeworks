2b6ef30419b2f71f058836f77e570e7c788482c5
Shpakov Rostislav <as2051990@yandex.ru>
2022-12-22 18:27:03 +0300

    Задачи 2 4 8

--- Program.cs
+++ Program.cs
@@ -18,6 +18,30 @@ else
    Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A); 
 }
 
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите 3 числа:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
+int num_3 = Convert.ToInt32(Console.ReadLine());
+int max = num_1;
+
+if (num_2 > max)
+{
+     max = num_2;
+}
+
+ if (num_3 > max)
+ {
+    max = num_3;
+ }
+
+ Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа
 // от 1 до N.
